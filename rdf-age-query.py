dafffec6eb0c02d92e6da0960f518792f851d93d
Al Crowley <albertcrowley@gmail.com>
2018-05-31 01:48:34 +0000

    sample python script that runs a simple RDF query

---
+++ rdf-age-query.py
@@ -0,0 +1,26 @@
+import rdflib
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+# parse command line arguments
+parser.add_argument('-nidm', dest='nidm_file', required=True, help="NIDM-Exp RDF File to import")
+args = parser.parse_args()
+
+
+g=rdflib.Graph()
+g.parse(args.nidm_file, format='ttl')
+
+
+qres = g.query(
+    """SELECT DISTINCT ?id ?age ?assessment   
+       WHERE {
+          ?assessment prov:wasGeneratedBy ?acq .
+          ?acq dct:isPartOf ?session .
+          ?acq prov:wasAssociatedWith ?person .
+          ?assessment ncicb:Age ?age .
+          ?person ndar:src_subject_id ?id
+       }""")
+
+
+for row in qres:
+    print("%s - %s - %s" % row)
